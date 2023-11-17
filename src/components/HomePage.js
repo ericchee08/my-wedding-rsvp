aa87cbc8b97c5d2107b51b2796f4111d0079180f
Eric Chee <ericchee@hotmail.co.uk>
2023-11-17 20:46:46 +0000

    Revert "test styles"
    This reverts commit e7ff858cb6ee9ef1e4cfd21feeffb78158f7f425.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import "../styles/HomePageStyle.css"
 import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
-    <>
+    <div className="home-page-container">
       <div className="header-container">
           <p id='title' >Eric & Lauren</p>
           <p id='date'>Monday, June 10, 2024</p>
@@ -14,7 +14,7 @@ const HomePage = () => {
             <p>Coming Soon...</p>
           </div>
       </div>
-    </>
+    </div>
   )
 }
 
