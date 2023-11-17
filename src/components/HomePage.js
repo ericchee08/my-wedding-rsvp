59904e8d40d0f244472f8cfefa1badced0bccc74
Eric Chee <ericchee@hotmail.co.uk>
2023-11-17 20:26:11 +0000

    update styling for home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,19 +2,19 @@ import "../styles/HomePageStyle.css"
 import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
-    <>
-    <div className="header-container">
-        <p id='title' >Eric and Lauren</p>
-        <p id='date'>Monday, June 10, 2024</p>
-        <Link to="/rsvp" id="rsvp"> RSVP</Link>
+    <div className="home-page-container">
+      <div className="header-container">
+          <p id='title' >Eric & Lauren</p>
+          <p id='date'>Monday, June 10, 2024</p>
+          <Link to="/rsvp" id="rsvp"> RSVP</Link>
+      </div>
+      <div className="container">
+          <div className="schedule-container">
+            <h2>Schedule</h2>
+            <p>Coming Soon...</p>
+          </div>
+      </div>
     </div>
-    <div className="container">
-        <div className="schedule-container">
-          <h2>Schedule</h2>
-          <p>Coming Soon...</p>
-        </div>
-    </div>
-    </>
   )
 }
 
