e6fe74130c66eaf826dc2dc8eb28a76ce71af8ff
Eric Chee <ericchee@hotmail.co.uk>
2023-11-24 20:57:52 +0000

    added additional info for confirmation page

--- src/components/Confirmations/AttendingConfirmationPage.js
+++ src/components/Confirmations/AttendingConfirmationPage.js
@@ -1,6 +1,27 @@
+import "./AttendingConfirmationPage.css"
+import { Link } from "react-router-dom";
+
 const AttendingConfirmationPage = () => {
   return (
-    <div>AttendingConfirmationPage</div>
+    <div>
+      <div className="attending-confirmation-page-container">
+        <div className="attending-confirmation-container">
+          <p className="rsvp-name">Eric & Lauren</p>
+          <p id="rsvp-venue"> The Riding School, Calke Abbey</p>
+          <p id="rsvp-date">Monday June 10 2024</p>
+          <p id="success-confirmation">You have succesfully sent your RSVP!</p>
+
+        </div>
+
+        <div id="back">
+              <Link to="/" id="back-button">
+                  Home
+              </Link>
+          </div>
+      </div>
+    </div>
+
+    
   )
 }
 
