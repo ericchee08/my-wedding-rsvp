77555a52d1319b5d119b04ef703ca5ab8a9dd489
Eric Chee <ericchee@hotmail.co.uk>
2023-11-24 21:13:08 +0000

    added font awesome plane icon

--- src/components/Confirmations/AttendingConfirmationPage.js
+++ src/components/Confirmations/AttendingConfirmationPage.js
@@ -1,5 +1,7 @@
 import "./AttendingConfirmationPage.css"
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane} from '@fortawesome/free-solid-svg-icons';
 
 const AttendingConfirmationPage = () => {
   return (
@@ -9,6 +11,7 @@ const AttendingConfirmationPage = () => {
           <p className="rsvp-name">Eric & Lauren</p>
           <p id="rsvp-venue"> The Riding School, Calke Abbey</p>
           <p id="rsvp-date">Monday June 10 2024</p>
+          <FontAwesomeIcon id='paper-plane'icon={faPaperPlane} bounce size="2xl" style={{ color: "#c067d5" }} />
           <p id="success-confirmation">You have succesfully sent your RSVP!</p>
 
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
+library.add(faPaperPlane);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
