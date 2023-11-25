a3a580661de86fd72bc723ed07fb3d7e2f17c08b
Eric Chee <ericchee@hotmail.co.uk>
2023-11-25 01:22:56 +0000

    theme style update

--- src/components/Confirmations/AttendingConfirmationPage.js
+++ src/components/Confirmations/AttendingConfirmationPage.js
@@ -11,9 +11,9 @@ const AttendingConfirmationPage = () => {
           
           <p className="confirmation-name">Eric & Lauren</p>
           <p className="confirmation-venue"> The Riding School, Calke Abbey <br /> Monday 10th June 2024</p>
-          <div className="paper-plane-container">
+            <div className="paper-plane-container">
           <FontAwesomeIcon id='paper-plane'icon={faPaperPlane} bounce size="2xl" style={{ color: "#D4B78A" }} />
-          </div>
+            </div>
           <p className="success-confirmation">Thanks for your RSVP ❤️ <br/> We can't wait to see you!</p>
           <div id="home">
               <Link to="/" id="home-button">
