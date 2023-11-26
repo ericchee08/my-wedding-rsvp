3fd39acfe550f7cd7ddb9eb6db4e9e0ade95f4f6
Eric Chee <ericchee@hotmail.co.uk>
2023-11-26 13:18:10 +0000

    rename style file name to include "Style" key word

--- src/components/Confirmations/AttendingConfirmationPage.js
+++ src/components/Confirmations/AttendingConfirmationPage.js
@@ -1,4 +1,4 @@
-import "../../styles/AttendingConfirmationPage.css"
+import "../../styles/AttendingConfirmationPageStyle.css"
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane} from '@fortawesome/free-solid-svg-icons';
